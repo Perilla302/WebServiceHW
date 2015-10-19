895dee9dfacfe81bfd5b9d3a82f9271f6df2693e
Hongjin Su <HongjinSu@hongjins-macbook-air.local>
2015-10-19 12:15:00 -0400

    Initial Commit

---
+++ WebServiceHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebServiceHW
+//
+//  Created by Hongjin Su on 10/19/15.
+//  Copyright © 2015 Hongjin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebServiceHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebServiceHW
+//
+//  Created by Hongjin Su on 10/19/15.
+//  Copyright © 2015 Hongjin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
